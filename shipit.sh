f4fafd5ff3b30850657ffb3d9458c0579c202a21
Alex Suraci <asuraci@pivotallabs.com>
2014-03-12 11:07:46 -0700

    add fake deploy pipeline logic
    Signed-off-by: David Varvel <dvarvel@pivotallabs.com>
    
    Conflicts:
    	.drone.yml

---
+++ shipit.sh
@@ -0,0 +1,35 @@
+set -e
+
+env
+
+function run_integration_tests() {
+  echo RUNNING INTEGRATION TESTS
+}
+
+function deploy() {
+  echo DEPLOYING
+}
+
+function run_system_tests() {
+  echo RUNNING SYSTEM TESTS
+}
+
+if [ ! -z "$DRONE_PR" ]; then
+  # don't do a full pipeline for pull requests!!!!!!!!!!!!!!!
+  run_integration_tests
+  exit 0
+fi
+
+case "$DRONE_BRANCH" in
+  master)
+    run_integration_tests
+    git push origin passed-integration
+    ;;
+  passed-integration)
+    deploy
+    git push origin passed-deploy
+    ;;
+  passed-deploy)
+    run_system_tests
+    ;;
+esac
